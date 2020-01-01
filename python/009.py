1b9a3678d68b1bf843d9b3cdef911adca7154d3e
藤平燎 <fujihiraryo@team-lab.com>
2020-01-01 16:00:56 +0900

    median filterまで

---
+++ python/009.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+
+def gaussian_filter(img, K=3, sigma=1.3):
+    H, W, C = img.shape
+    # padding
+    pad = K//2
+    img2 = np.zeros((H+pad*2, W+pad*2, C), dtype=np.float)
+    img2[pad:pad+H, pad:pad+W] = img.copy().astype(np.float)
+    # kernel
+    kernel = np.zeros((K, K), dtype=np.float)
+    for x in range(-pad, -pad+K):
+        for y in range(-pad, -pad+K):
+            kernel[y+pad, x+pad] = np.exp(-(x**2+y**2)/(2*(sigma**2)))
+    kernel /= (2*np.pi*(sigma**2))
+    kernel /= kernel.sum()
+    # filtering
+    img3 = img2.copy()
+    for h in range(H):
+        for w in range(W):
+            for c in range(C):
+                img3[h+pad, w+pad, c] = np.sum(kernel*img2[h:h+K, w:w+K, c])
+    img3 = np.clip(img3, 0, 255)
+    img3 = img3[pad:pad+H, pad:pad+W].astype(np.uint8)
+    return img3
+
+
+img = cv2.imread("image/imori_noise.jpg")
+img2 = gaussian_filter(img)
+cv2.imwrite("image/009.jpg", img2)
