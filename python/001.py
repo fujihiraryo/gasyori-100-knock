c169f5086d6c29c88a489e9a7e3546429d4551c7
fujihiraryo <fujihiraryo.team-lab.com>
2021-10-16 21:18:13 +0900

    update gitignore

--- python/001.py
+++ python/001.py
@@ -1,7 +1,6 @@
 import cv2
-img=cv2.imread("image/eevee.png")
-H,W,C=img.shape
-for h in range(H):
-    for w in range(W):
-        img[h][w][2]=img[h][w][0]
-cv2.imwrite("image/001.png",img)
+
+image = cv2.imread("image/sample.png")
+h, w, _ = image.shape
+image[:, :, 0], image[:, :, 2] = image[:, :, 2], image[:, :, 0]
+cv2.imwrite("image/001.png", image)
